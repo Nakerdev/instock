f22086a615a69cec8819e8806ad12e834be349e1
Antonio Sánchez <antoniojesussg96@gmail.com>
2021-10-23 15:15:37 +0200

    kibt

--- webapp/infraestructure/notifications/emails/sendGridMailSender.ts
+++ webapp/infraestructure/notifications/emails/sendGridMailSender.ts
@@ -1,31 +1,29 @@
 import sendGridMail from '@sendgrid/mail'
 
-import MailService, { MailSendingRequest } from "../../../business/infraestructure/mailService";
+import MailService, { MailSendingRequest } from '../../../business/infraestructure/mailService'
 
 export default class SendGridMailService implements MailService {
+  constructor (sendGridApiKey: string) {
+    sendGridMail.setApiKey(sendGridApiKey)
+  }
 
-    constructor(sendGridApiKey: string) {
-        sendGridMail.setApiKey(sendGridApiKey)
+  send (request: MailSendingRequest): Promise<void> {
+    const msg = {
+      to: request.to,
+      from: request.from,
+      subject: request.subject,
+      text: '',
+      html: request.html
     }
 
-    send(request: MailSendingRequest): Promise<void> {
-        const msg = {
-            to: request.to,
-            from: request.from,
-            subject: request.subject,
-            text: '',
-            html: request.html,
-        }
-
-        return sendGridMail
-            .send(msg)
-            .then((response) => {
-                console.log(response[0].statusCode)
-                console.log(response[0].headers)
-            })
-            .catch((error) => {
-                console.error(error)
-            })
-    }
-
+    return sendGridMail
+      .send(msg)
+      .then((response) => {
+        console.log(response[0].statusCode)
+        console.log(response[0].headers)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
 }
